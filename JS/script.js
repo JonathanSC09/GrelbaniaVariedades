67ff0eff936b08fd9f94f9fc12a48685f919ad75
JonathanSC09 <126822871+JonathanSC09@users.noreply.github.com>
2023-05-18 20:58:37 -0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+/*NAV*/
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+/*NAV END*/
+/*SINGLE PROD*/
+
+/*SINGLE PROD END*/
